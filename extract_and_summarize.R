219f87b7a85080e2a188779bc84f4461aa0db398
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2021-12-10 13:18:33 +0530

    Update extract_and_summarize.R

--- extract_and_summarize.R
+++ extract_and_summarize.R
@@ -1,2 +1,5 @@
+#Extract data from ASCTB and Azimuth and store their cell IDs and names
 source('extract_cts.R')
+
+# Take a set difference between ASCTB cell types and Azimuth celltypes and store the results in csvs
 source('merge_cts.R')
